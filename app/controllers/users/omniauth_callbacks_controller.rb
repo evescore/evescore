e99d29bc1215da3bdfe1b92b19c32564ffce3cb1
EVEScore <evescore@evescore.online>
2018-07-13 01:54:30 +0200

    correcting a botched callback url

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  def crest
+  def esi
     redirect_to root_path && return unless current_user
     omniauth_payload = request.env['omniauth.auth']
     current_user.import_character(omniauth_payload)
