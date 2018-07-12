bdb1743fb6b43dee2f0f8ba73ffbe22b0923212e
EVEScore <evescore@evescore.online>
2018-07-12 20:30:26 +0200

    capistrano whenever

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
+set :whenever_command, "bundle exec whenever"
+require "whenever/capistrano"
 
 # config valid for current version and patch releases of Capistrano
 lock '~> 3.11.0'
